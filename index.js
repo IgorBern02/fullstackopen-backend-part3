e70d41440ae93991dd706262142c47fb59a67ce2
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-05 17:41:21 -0300

    Add exercise38 from phonebook backend

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+morgan.token("body", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
 app.use(express.json());
-app.use(morgan("tiny"));
 
 let persons = [
   {
