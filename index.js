259d67cd7e67b5483be911284ba43cb8c8566fd6
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-05 16:34:14 -0300

    Add exercise34 from phonebook backend

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  response.status(204).end();
+});
+
 app.use(express.json());
 
 const PORT = 3001;
