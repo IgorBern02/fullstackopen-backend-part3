84c791c045d05d4ebd55ad54b00b97b6d8ed939d
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-05 15:56:00 -0300

    feat: setup part3

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+//
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "application/json" });
+//   res.end(JSON.stringify(notes));
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
