d6d59c86e962383500f016fcbd7551922338c5e5
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-09 15:40:25 -0300

    Add exercise315 from phonebook database

--- index.js
+++ index.js
@@ -42,70 +42,21 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      console.error("Error deleting person:", error);
+      response.status(500).json({ error: "Failed to delete person" });
+    });
+});
+
+// Iniciar servidor (caso você queira testar localmente com nodemon)
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// app.get("/info", (request, response) => {
-//   const date = new Date();
-//   Person.countDocuments({}).then((count) => {
-//     const info = `<p>Phonebook has info for ${count} people</p>
-//                   <p>${date}</p>`;
-//     response.send(info);
-//   });
-// });
-
-// app.get("/api/persons/:id", (request, response) => {
-//   Person.findById(request.params.id)
-//     .then((person) => {
-//       if (person) {
-//         response.json(person);
-//       } else {
-//         response.status(404).end();
-//       }
-//     })
-//     .catch(() => {
-//       response.status(400).send({ error: "malformatted id" });
-//     });
-// });
-
-// app.post("/api/persons", (request, response) => {
-//   const body = request.body;
-
-//   if (!body.name || !body.number) {
-//     return response.status(400).json({ error: "name or number missing" });
-//   }
-
-//   Person.findOne({ name: body.name }).then((existingPerson) => {
-//     if (existingPerson) {
-//       return response.status(400).json({ error: "name must be unique" });
-//     }
-
-//     const person = new Person({
-//       name: body.name,
-//       number: body.number,
-//     });
-
-//     person.save().then((savedPerson) => {
-//       response.json(savedPerson);
-//     });
-//   });
-// });
-
-// app.delete("/api/persons/:id", (request, response) => {
-//   Person.findByIdAndDelete(request.params.id)
-//     .then(() => {
-//       response.status(204).end();
-//     })
-//     .catch(() => {
-//       response.status(400).send({ error: "malformatted id" });
-//     });
-// });
-
-// Rota catch-all para SPA (deve ser a última)
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "build", "index.html"));
-// });
-
-// Iniciar servidor (caso você queira testar localmente com nodemon)
